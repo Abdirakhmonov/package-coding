fd1ed95c4ba16ba205d9fd22954cc843e864862f
Abdirakhmonov <abdirahmonovakramjon@mail.ru>
2024-08-05 12:11:22 +0500

    last commit

--- lib/najot_coding.dart
+++ lib/najot_coding.dart
@@ -1,4 +1,4 @@
 library najot_coding;
 
-export 'src/najot_coding_service.dart';
 export 'src/najot_coding_base.dart';
+export 'src/najot_coding_service.dart';
--- test/najot_coding_test.dart
+++ test/najot_coding_test.dart
@@ -1,16 +1,33 @@
-import 'package:najot_coding/najot_coding.dart';
 import 'package:test/test.dart';
+import 'package:najot_coding/najot_coding.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
+  group('NajotCodingService Tests', () {
+    final keyString = 'your-32-character-key';
+    final najotCodingService = NajotCodingService(keyString);
+    final plainText = 'Hello, Najot Coding!';
+    late String encryptedText;
 
     setUp(() {
-      // Additional setup goes here.
+      // Additional setup if needed.
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('Encrypt Test', () {
+      encryptedText = najotCodingService.encrypt(plainText);
+      expect(encryptedText, isNotNull);
+      expect(encryptedText, isNot(plainText));
+    });
+
+    test('Decrypt Test', () {
+      encryptedText = najotCodingService.encrypt(plainText);
+      final decryptedText = najotCodingService.decrypt(encryptedText);
+      expect(decryptedText, equals(plainText));
+    });
+
+    test('Encryption and Decryption Consistency', () {
+      encryptedText = najotCodingService.encrypt(plainText);
+      final decryptedText = najotCodingService.decrypt(encryptedText);
+      expect(decryptedText, equals(plainText));
     });
   });
 }
