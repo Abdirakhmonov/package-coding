911d08db7a4209ce10889bb715fb112c42b0c1db
Abdirakhmonov <abdirahmonovakramjon@mail.ru>
2024-08-05 12:48:15 +0500

    last commit

--- lib/src/najot_coding_service.dart
+++ lib/src/najot_coding_service.dart
@@ -6,28 +6,44 @@ class NajotCodingService {
   final KeyParameter keyParam;
   final PaddedBlockCipher cipher;
 
+  // Constructor initializes the KeyParameter and PaddedBlockCipher
   NajotCodingService(String keyString)
-      : keyParam = KeyParameter(Uint8List.fromList(utf8.encode(keyString.padRight(32, ' ')))),
+      : keyParam = KeyParameter(
+          Uint8List.fromList(
+            utf8.encode(
+              keyString.padRight(32,
+                  ' '), // Ensure the key is 32 bytes long by padding with spaces
+            ),
+          ),
+        ),
         cipher = PaddedBlockCipher('AES/CBC/PKCS7') {
-    final iv = Uint8List(16); // Initialization vector
-    final params = PaddedBlockCipherParameters<CipherParameters, CipherParameters>(
-        ParametersWithIV<KeyParameter>(keyParam, iv), null);
-    cipher.init(true, params);
+    final iv = Uint8List(
+        16); // Initialization vector for AES (should be unique for each encryption)
+    final params =
+        PaddedBlockCipherParameters<CipherParameters, CipherParameters>(
+            ParametersWithIV<KeyParameter>(keyParam, iv), null);
+    cipher.init(true, params); // Initialize cipher for encryption
   }
 
+  // Encrypts the plain text using AES
   String encrypt(String plainText) {
-    final input = Uint8List.fromList(utf8.encode(plainText));
-    final encrypted = cipher.process(input);
-    return base64Encode(encrypted);
+    final input = Uint8List.fromList(
+        utf8.encode(plainText)); // Convert plain text to bytes
+    final encrypted = cipher.process(input); // Encrypt the bytes
+    return base64Encode(encrypted); // Encode encrypted bytes as base64
   }
 
+  // Decrypts the encrypted text using AES
   String decrypt(String encryptedText) {
-    final iv = Uint8List(16); // Initialization vector
-    final params = PaddedBlockCipherParameters<CipherParameters, CipherParameters>(
-        ParametersWithIV<KeyParameter>(keyParam, iv), null);
-    cipher.init(false, params);
-    final encrypted = base64Decode(encryptedText);
-    final decrypted = cipher.process(encrypted);
-    return utf8.decode(decrypted);
+    final iv = Uint8List(
+        16); // Initialization vector for AES (should match the one used during encryption)
+    final params =
+        PaddedBlockCipherParameters<CipherParameters, CipherParameters>(
+            ParametersWithIV<KeyParameter>(keyParam, iv), null);
+    cipher.init(false, params); // Initialize cipher for decryption
+    final encrypted =
+        base64Decode(encryptedText); // Decode base64-encoded encrypted text
+    final decrypted = cipher.process(encrypted); // Decrypt the bytes
+    return utf8.decode(decrypted); // Convert decrypted bytes back to string
   }
 }
